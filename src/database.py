f8330686d6a52f69936eb1995315573a87ebe39e
VS-tyl <vishesh.shah@cignex.com>
2025-11-05 17:22:25 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/file-manager"
+DATABASE_URL = "URLFORDATABASE"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
